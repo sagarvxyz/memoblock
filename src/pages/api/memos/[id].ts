6a45694fe447d32c01cd298c4f5ecdfc15a3c67e
sagarvxyz <sagar.velagala@gmail.com>
2023-02-14 18:51:05 -0500

    feat: mvp api setup

---
+++ src/pages/api/memos/[id].ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const data = await GET(req, res);
+    res.json(data);
+  }
+}
+
+async function GET(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = 'GET worked';
+    return res.json(data);
+  } catch (error) {
+    if (!(error instanceof Error)) {
+      throw new Error('Unknown server error');
+    }
+    console.log(error.message);
+    return res.status(500).json(error.message);
+  }
+}
+
+async function POST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = 'POST worked';
+    return res.json(data);
+  } catch (error) {
+    if (!(error instanceof Error)) {
+      throw new Error('Unknown server error');
+    }
+    console.log(error.message);
+    return res.status(500).json(error.message);
+  }
+}
