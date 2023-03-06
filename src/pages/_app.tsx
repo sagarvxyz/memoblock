163e693c625d0321347a18549f9814efe7c69479
sagarvxyz <sagar.velagala@gmail.com>
2023-03-06 17:08:28 -0500

    chore: uninstall tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
-// import '@common/styles.css';
+import '@common/styles.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
